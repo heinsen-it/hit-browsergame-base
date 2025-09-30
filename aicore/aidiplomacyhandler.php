3941702c2bcaea76a75d730fa44cf6b1f01c869f
Kjell Heinsen <ich@kjell-heinsen.de>
2025-09-30 23:59:44 +0200

    Add Relationship Status

--- aicore/aidiplomacyhandler.php
+++ aicore/aidiplomacyhandler.php
@@ -32,4 +32,51 @@ class aidiplomacyhandler{
 
     }
 
+
+    /**
+     * Initialisiert die Schwellenwerte für verschiedene Beziehungsstufen
+     *
+     * @return void
+     */
+    private function initializeOpinionThresholds(): void {
+        $this->opinionThresholds = [
+            'war' => -75,           // Unter -75: Kriegszustand
+            'hostile' => -25,       // -75 bis -25: Feindlich
+            'unfriendly' => -5,     // -25 bis -5: Unfreundlich
+            'neutral' => 5,         // -5 bis 5: Neutral
+            'cordial' => 25,        // 5 bis 25: Herzlich
+            'friendly' => 50,       // 25 bis 50: Freundlich
+            'allied' => 75          // Über 75: Verbündet
+        ];
+    }
+
+
+
+
+
+
+    /**
+     * Gibt den Beziehungsstatus basierend auf dem Meinungswert zurück
+     *
+     * @param int $opinion Meinungswert
+     * @return string Beziehungsstatus
+     */
+    private function getRelationshipStatus(int $opinion): string {
+        if ($opinion <= $this->opinionThresholds['war']) {
+            return 'war';
+        } elseif ($opinion <= $this->opinionThresholds['hostile']) {
+            return 'hostile';
+        } elseif ($opinion <= $this->opinionThresholds['unfriendly']) {
+            return 'unfriendly';
+        } elseif ($opinion <= $this->opinionThresholds['neutral']) {
+            return 'neutral';
+        } elseif ($opinion <= $this->opinionThresholds['cordial']) {
+            return 'cordial';
+        } elseif ($opinion <= $this->opinionThresholds['friendly']) {
+            return 'friendly';
+        } else {
+            return 'allied';
+        }
+    }
+
 }
